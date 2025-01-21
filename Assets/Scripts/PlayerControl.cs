e46adbc41aa71b4fd4f44db3f68a53e2dd87498b
DamianPare <damianpare@shaw.ca>
2025-01-21 04:43:35 -0800

    Added Shadow Meshes
    Shadow meshes are now created dynamically, still need to link generated meshes to tp mechanic

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -30,8 +30,6 @@ public class PlayerControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(IsGrounded());
-
         if (IsGrounded())
         {
             UpdateVelocity();
