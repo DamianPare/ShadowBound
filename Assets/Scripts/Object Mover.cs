9291724a203f8f3d56ec4bd209c7d1b3a9a0105d
DamianPare <damianpare@shaw.ca>
2025-02-05 18:40:26 -0800

    Added Rotation to lights
    Added myBox Package

--- Assets/Scripts/Object Mover.cs
+++ Assets/Scripts/Object Mover.cs
@@ -3,25 +3,44 @@ using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 using Cinemachine.Utility;
+using MyBox.Internal;
+using MyBox;
 
 public class ObjectMover : MonoBehaviour
 {
     [SerializeField] private GameObject objectToMove;
-    [SerializeField] private Vector3 distanceToMove;
+
+    public bool moveObject;
+    [ConditionalField("moveObject")] public Vector3 distanceToMove;
+
+    public bool rotateObject;
+    [ConditionalField("rotateObject")] public Quaternion targetAngle;
+
     [SerializeField] private float timeToMove;
 
     private Vector3 startPos;
+    private Quaternion startAngle;
 
     private void Awake()
     {
         startPos = objectToMove.transform.position;
+        startAngle = objectToMove.transform.rotation;
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.layer == 9)
         {
-            MoveObject();
+            if (moveObject)
+            {
+                MoveObject();
+            }
+           
+
+            if (rotateObject)
+            {
+                RotateObject();
+            }
         }
     }
 
@@ -35,6 +54,19 @@ public class ObjectMover : MonoBehaviour
         else
         {
             objectToMove.transform.DOMove(startPos, timeToMove);
+        }
+    }
+
+    void RotateObject()
+    {
+        if (objectToMove.transform.rotation == startAngle)
+        {
+            objectToMove.transform.DORotate(objectToMove.transform.rotation * targetAngle.eulerAngles, timeToMove);
+        }
+
+        else
+        {
+            objectToMove.transform.DORotate(startAngle.eulerAngles, timeToMove);
         }
     }
 }
