bc99d88d94eecba83a5d840a3163eda22b171692
LuisVivancoG <48226613+LuisVivancoG@users.noreply.github.com>
2025-02-06 08:08:37 -0800

    Build settings

--- Assets/Scripts/Shooting.cs
+++ Assets/Scripts/Shooting.cs
@@ -1,8 +1,5 @@
 using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.EditorTools;
 using UnityEngine;
-using UnityEngine.Events;
 
 public class Shooting : MonoBehaviour
 {
